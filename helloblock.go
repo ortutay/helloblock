6cf5029369519e88c5412ee4a31488916fc71c4f
ortutay <marcell.ortutay@gmail.com>
2014-08-22 17:56:27 -0700

    getaddress

--- helloblock.go
+++ helloblock.go
@@ -9,6 +9,29 @@ import (
 	"strings"
 )
 
+type HelloBlockAddress struct {
+	Balance uint64 `json:"balance"`
+	ConfirmedBalance uint64 `json:"confirmedBalance"`
+	TxsCount uint `json:"txsCount"`
+	ConfirmedTxsCount uint `json:"confirmedTxsCount"`
+	TotalReceivedValue uint64 `json:"totalReceivedValue"`
+	ConfirmedReceivedValue uint64 `json:"confirmedReceivedValue"`
+	Address string `json:"address"`
+	Hash160 string `json:"hash160"`
+	Type string `json:"type"`
+}
+
+type HelloBlockGetAddressData struct {
+	Address HelloBlockAddress `json:"address"`
+}
+
+type HelloBlockReply struct {
+	Status  string               `json:"status"`
+	Message string               `json:"message"`
+	Details []string             `json:"details"`
+	DataRaw    json.RawMessage `json:"data"`
+}
+
 type HelloBlockUnspent struct {
 	TxHash       string `json:"txHash"`
 	Index        int    `json:"index"`
@@ -70,9 +93,42 @@ func SetNetwork(newNetwork HelloBlockNetwork) {
 	network = newNetwork
 }
 
+func GetAddress(address string) (*HelloBlockGetAddressData, error) {
+	u := fmt.Sprintf("%s/%s", helloBlockURL("v1/addresses"), address)
+	reply, err := getReply(u)
+	if err != nil {
+		return nil, err
+	}
+	var data HelloBlockGetAddressData
+	fmt.Printf("data %v\n", reply.DataRaw)
+	if err := json.Unmarshal(reply.DataRaw, &data); err != nil {
+		return nil, fmt.Errorf("couldn't process JSON %v: %v", reply.DataRaw, err)
+	}
+	return &data, nil
+}
+
+func getReply(u string) (*HelloBlockReply, error) {
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, fmt.Errorf("error calling %v: %v", u, err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var reply HelloBlockReply
+	if err := json.Unmarshal(body, &reply); err != nil {
+		return nil, fmt.Errorf("could not process JSON %v: %v", string(body), err)
+	}
+	if reply.Status != success {
+		return nil, fmt.Errorf("error from %v: %v: %v", u, reply.Message, strings.Join(reply.Details, ", "))
+	}
+	return &reply, nil
+}
+
 func Faucet(typ int) (*HelloBlockFaucetData, error) {
-	u := helloBlockURL("v1/faucet")
-	resp, err := http.Get(fmt.Sprintf("%s?type=%d", u, typ))
+	u := fmt.Sprintf("%s?type=%d", helloBlockURL("v1/faucet"), typ)
+	resp, err := http.Get(u)
 	if err != nil {
 		return nil, fmt.Errorf("error calling %v: %v", u, err)
 	}
@@ -85,7 +141,7 @@ func Faucet(typ int) (*HelloBlockFaucetData, error) {
 		return nil, fmt.Errorf("could not process JSON %v: %v", string(body), err)
 	}
 	if reply.Status != success {
-		return nil, fmt.Errorf("error from helloblock.io: %v: %v", reply.Message, strings.Join(reply.Details, ", "))
+		return nil, fmt.Errorf("error from %v: %v: %v", u, reply.Message, strings.Join(reply.Details, ", "))
 	}
 	return &reply.Data, nil
 }
